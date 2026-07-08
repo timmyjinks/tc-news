0a98b012cdd18eee434ca2b307ab938699cb937e
timmyjinks <tysonjjenkins.github@gmail.com>
2026-07-07 21:52:34 -0600

    port comment services from go to python

--- comment/kafka/model.go
+++
@@ -1,9 +0,0 @@
-package kafka
-
-import "encoding/json"
-
-type Message struct {
-	ID      string          `json:"id"`
-	Type    string          `json:"type"`
-	Payload json.RawMessage `json:"payload"`
-}
